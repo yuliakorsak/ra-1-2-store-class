bd0f4650208a0a32ac98d1fce9d3bebc64f342be
Yulia Ermolaeva <168757304+yuliakorsak@users.noreply.github.com>
2024-10-31 00:38:07 +0300

    React App created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import { Component } from 'react';
+
+class ShopItemClass extends Component {
+  render() {
+    const { item } = this.props;
+    return (
+      <div className="main-content">
+        <h2>{item.brand}</h2>
+        <h1>{item.title}</h1>
+        <h3>{item.description}</h3>
+        <div className="description">{item.descriptionFull}</div>
+        <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+        <div className="divider"></div>
+        <div className="purchase-info">
+          <div className="price">{item.currency}{item.price.toFixed(2)}</div>
+          <button>Добавить в корзину</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+const item = {
+  brand: 'Tiger of Sweden',
+  title: 'Leonard coat',
+  description: 'Minimalistic coat in cotton-blend',
+  descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+  price: 399,
+  currency: '£'
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="background-element">
+      </div>
+      <div className="highlight-window">
+        <div className='highlight-overlay'></div>
+      </div>
+      <div className="window">
+        <ShopItemClass item={item} />
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
